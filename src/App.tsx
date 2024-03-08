a4814a28eb50cb912c1dc0721e7278f64013cd19
jokinz <53506251+jokinz@users.noreply.github.com>
2024-03-08 01:00:28 -0500

    added shadcn configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import './App.css'
 
+import { ThemeProvider } from '@/components/theme-provider'
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+
 import PlanDePago from './components/PlanDePago'
 import TraspasoDeBienesYBaja from './components/TraspasoDeBienesYBaja'
 
 function App() {
   return (
     <>
-      <PlanDePago />
-      <TraspasoDeBienesYBaja />
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <Tabs defaultValue="paymentPlan">
+          <TabsList>
+            <TabsTrigger value="paymentPlan">
+              Aplicación de plan de pago
+            </TabsTrigger>
+            <TabsTrigger value="goodsTransfer">
+              Traspaso de bienes y baja
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="paymentPlan">
+            <PlanDePago />
+          </TabsContent>
+          <TabsContent value="goodsTransfer">
+            <TraspasoDeBienesYBaja />
+          </TabsContent>
+        </Tabs>
+      </ThemeProvider>
     </>
   )
 }
