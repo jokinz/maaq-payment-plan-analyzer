63f37901614b795a402ef6b3f9711824ad426efd
jokinz <53506251+jokinz@users.noreply.github.com>
2024-04-30 12:45:51 -0500

    Update Sheet.tsx

--- src/components/Sheet.tsx
+++ src/components/Sheet.tsx
@@ -1,57 +1,27 @@
-import { getCellFunction } from '@/Utils'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
+import FormField from './FormField'
 import { Checkbox } from './ui/checkbox'
 import { Label } from './ui/label'
-import FormField from './FormField'
 
-type props = {
-  sheetName: string
-  file: File
+export type sheetProps = {
+  name: string
+  checked: boolean
+  paymentsQuantity: number
+  updateList: (index: number) => void
 }
 
-const targetSheetNames: string[] = [
-  'vehiculo',
-  'seguro vehicula',
-  'seguro de vida',
-]
-
-const webpcf = 'WEBPCF'
-
-const Sheet = ({ sheetName, file }: props) => {
-  const [contains, setContains] = useState<boolean>(false)
+const Sheet = (
+  { name, checked, paymentsQuantity, updateList }: sheetProps,
+  key: number
+) => {
   const [starterCell, setStarterCell] = useState<string>('C28')
-
-  useEffect(() => {
-    const cellContainsString = async () => {
-      try {
-        const cellFunction = (await getCellFunction(
-          file,
-          webpcf,
-          'E10'
-        )) as string
-        if (
-          cellFunction.includes(sheetName) &&
-          (cellFunction.includes(targetSheetNames[0]) ||
-            cellFunction.includes(targetSheetNames[1]) ||
-            cellFunction.includes(targetSheetNames[2]))
-        ) {
-          setContains(true)
-        }
-      } catch (error) {
-        console.error(error)
-      }
-    }
-    cellContainsString()
-  }, [])
 
   return (
     <div className="grid grid-cols-4 gap-4 text-left items-center">
-      <Checkbox
-        id={sheetName}
-        checked={contains}
-        onClick={() => setContains(!contains)}
-      />
-      <Label htmlFor={sheetName}>{sheetName}</Label>
+      <Checkbox id={name} checked={checked} onClick={() => updateList(key)} />
+      <Label htmlFor={name}>
+        {name}({paymentsQuantity})
+      </Label>
       <FormField
         htmlFor="starterCell"
         label="Celda Inicial"
