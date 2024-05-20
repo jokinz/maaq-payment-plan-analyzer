cdad3ae58e88c41ac885f184859d3ec5768548ec
jokinz <53506251+jokinz@users.noreply.github.com>
2024-05-20 17:17:38 -0500

    Update Queries.ts

--- src/Queries.ts
+++ src/Queries.ts
@@ -92,3 +92,17 @@ export const updateOperationPaymentsQuery = (
   const query: string = `UPDATE SCA_HIPOTEC..COL SET FLD_COL_NDOC = '${updateNumber}' WHERE FLD_COL_OPER = ${operationNumber} AND FLD_COL_NCU = ${paymentNumber};\n`
   return query
 }
+
+export const createBackupQuery = (date: Date): string => {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  const query: string = `use BT_SFCO
+  GO
+  
+  SELECT * 
+  INTO COL_${year}${month}${day}
+  FROM SCA_HIPOTEC..COL`
+  return query
+}
