9521cc92d7ab146dc693647b049223fdb11ed21c
jokinz <53506251+jokinz@users.noreply.github.com>
2024-06-26 11:36:08 -0500

    removed unused functions

--- src/components/PlanDePagoAdv.tsx
+++ src/components/PlanDePagoAdv.tsx
@@ -9,7 +9,7 @@ import {
   getCellValue,
   getColumnFormulas,
   getSheetsProps,
-  readFile
+  readFile,
 } from '@/Utils'
 
 import Wrapper from '@/components/Wrapper'
@@ -32,7 +32,6 @@ export type queryData = {
   saldo: number
 }
 
-
 const webpcf: string = 'WEBPCF'
 const cellOperationNumber: string = 'C4'
 const starterFunctionCellLocation: string = 'E10'
@@ -54,144 +53,11 @@ const PlanDePagoAdv = () => {
 
   const fileRef = useRef<HTMLInputElement>(null)
 
-  const getValidSheetsNames = async (file: File) => {
-    const validSheets: string[] = []
-    try {
-      const workbook = await readFile(file)
-
-      workbook.SheetNames.forEach((sheetName) => {
-        const sheet = workbook.Sheets[sheetName]
-        const rowsToCheck: number = 100
-        let isValidSheet: boolean = false
-
-        for (let rowNum = 0; rowNum < rowsToCheck; rowNum++) {
-          const header1Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 0 })]
-          const header2Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 1 })]
-          const header3Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 2 })]
-          const header4Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 3 })]
-          const header5Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 4 })]
-          const header6Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 5 })]
-          const header7Cell = sheet[XLSX.utils.encode_cell({ r: rowNum, c: 6 })]
-
-          if (
-            !header1Cell &&
-            !header2Cell &&
-            !header3Cell &&
-            !header4Cell &&
-            !header5Cell &&
-            !header6Cell
-          ) {
-            continue
-          }
-
-          const header1 = header1Cell ? header1Cell.v : ''
-          const header2 = header2Cell ? header2Cell.v : ''
-          const header3 = header3Cell ? header3Cell.v : ''
-          const header4 = header4Cell ? header4Cell.v : ''
-          const header5 = header5Cell ? header5Cell.v : ''
-          const header6 = header6Cell ? header6Cell.v : ''
-          if (header6 !== pattern[5] && header7Cell) {
-            const header7 = header7Cell ? header7Cell.v : ''
-            if (
-              header1 == pattern[0] &&
-              header2 == pattern[1] &&
-              header3 == pattern[2] &&
-              header4 == pattern[3] &&
-              header5 == pattern[4] &&
-              header7 == pattern[5]
-            ) {
-              isValidSheet = true
-              continue
-            }
-          } else {
-            if (
-              header1 == pattern[0] &&
-              header2 == pattern[1] &&
-              header3 == pattern[2] &&
-              header4 == pattern[3] &&
-              header5 == pattern[4] &&
-              header6 == pattern[5]
-            ) {
-              isValidSheet = true
-              continue
-            }
-          }
-        }
-        if (isValidSheet) {
-          validSheets.push(sheetName)
-        }
-        return validSheets
-      })
-    } catch (error) {
-      alert(error)
-    }
-    return validSheets
-  }
-
   const checkIfSelectedSheetsMissType = () => {
     const result: boolean = sheetsList
       .filter((sheet: sheetProps) => sheet.checked)
       .some((sheet: sheetProps) => sheet.type === 0)
     return result
-  }
-
-  const getSheetDataOld = async (
-    file: File,
-    sheetName: string,
-    tipo: number,
-    columnName: string = 'A'
-  ): Promise<any> => {
-    try {
-      const workbook = await readFile(file)
-      const sheet = workbook.Sheets[sheetName]
-
-      const columnRange = XLSX.utils.decode_range(sheet['!ref'] as string)
-      const colIndex = XLSX.utils.decode_col(columnName)
-      let data: queryData[] = []
-      for (
-        let rowIndex = columnRange.s.r;
-        rowIndex <= columnRange.e.r;
-        rowIndex++
-      ) {
-        const cellAddress = { r: rowIndex, c: colIndex }
-        const cellRef = XLSX.utils.encode_cell(cellAddress)
-        const nroCuota = sheet[cellRef]?.v
-        if (nroCuota !== undefined && typeof nroCuota === 'number') {
-          const fecha =
-            sheet[XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 1 })]?.v
-          const saldo =
-            sheet[XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 2 })]?.v
-          const intereses =
-            sheet[XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 3 })]?.v
-          const capital = sheet[
-            XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 4 })
-          ]?.v
-            ? sheet[XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 4 })]?.v
-            : 0
-          const cuota = sheet[
-            XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 6 })
-          ]?.v
-            ? sheet[XLSX.utils.encode_cell({ r: rowIndex, c: colIndex + 6 })]?.v
-            : 0
-
-          const rowData: queryData = {
-            tipo,
-            nroCuota,
-            fecha,
-            cuota,
-            capital,
-            intereses,
-            saldo,
-          }
-          data = [...data, rowData]
-        }
-      }
-      return data
-    } catch (error) {
-      alert(error)
-    }
-
-    return
   }
 
   const getSheetData = async (
