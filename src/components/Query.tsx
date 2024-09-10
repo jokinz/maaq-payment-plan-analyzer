85e1f0853fe3ae1270beb73bb5e976f6783f19ff
jokinz <53506251+jokinz@users.noreply.github.com>
2024-09-10 16:13:04 -0300

    updated Query styles

--- src/components/Query.tsx
+++ src/components/Query.tsx
@@ -15,7 +15,7 @@ function Query({ content }: props) {
         style={{ gridColumn: 'span 12 / span 12' }}
         disabled
         value={content}
-        className="textarea-query min-h-[150] resize-y"
+        className="textarea-query min-h-auto resize-y"
       ></Textarea>
       <Button
         style={{ gridColumn: '12 / span 1' }}
