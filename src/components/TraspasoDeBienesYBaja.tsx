db19e6fff0ba5a7c381bf829d7bea783e8eb615c
jokinz <53506251+jokinz@users.noreply.github.com>
2026-03-31 14:35:43 -0500

    updated logic and ui

--- src/components/TraspasoDeBienesYBaja.tsx
+++ src/components/TraspasoDeBienesYBaja.tsx
@@ -20,7 +20,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import FormField from '@/components/FormField'
 import Wrapper from '@/components/Wrapper'
 import BackUp from './BackUp'
-import { goodsBackupQuery } from '@/Queries'
+import { goodsBackupQuery, transferGoodsQuery } from '@/Queries'
 
 type Status = {
   value: string
@@ -46,6 +46,9 @@ function TraspasoDeBienesYBaja() {
   const [sourceOperation, setSourceOperation] = useState<number>(0)
   const [targetOperation, setTargetOperation] = useState<number>(0)
   const [selectedStatus, setSelectedStatus] = useState<string>('')
+  const [includeStatus, setIncludeStatus] = useState<'include' | 'exclude'>(
+    'include'
+  )
   const [good, setGood] = useState<string>('')
   const [goodsList, setGoodsList] = useState<string[]>([])
 
@@ -129,6 +132,20 @@ DELETE FROM SCA_ADMINI..GAR WHERE FLD_GAR_OPER = ${targetOperation}`
                 }
                 name="selectedStatus"
               >
+                <SelectTrigger>
+                  <SelectValue placeholder="Seleccionar hoja" />
+                </SelectTrigger>
+                <SelectContent>
+                  <SelectItem key={1} value={'include'}>
+                    Incluir solo:
+                  </SelectItem>
+                  <SelectItem key={2} value={'exclude'}>
+                    Excluir bienes:
+                  </SelectItem>
+                </SelectContent>
+              </Select>
+            </div>
+            <div className='col-span-10 text-left gap-2 flex'>
               {goodsList.map((item, index) => (
                 <Button
                   key={index}
@@ -143,32 +160,17 @@ DELETE FROM SCA_ADMINI..GAR WHERE FLD_GAR_OPER = ${targetOperation}`
                   {item} X
                 </Button>
               ))}
+            </div>
           </div>
         )}
 
         <Query
-          content={
-            targetOperation && sourceOperation
-              ? `INSERT INTO SCA_ADMINI..GAR
-       SELECT ${targetOperation} , FLD_GAR_NCHASIS,FLD_GAR_NMOT,FLD_GAR_MODB,FLD_GAR_TIPB,FLD_GAR_ESTB,FLD_GAR_PRD,FLD_GAR_SUC,FLD_GAR_MON
-,FLD_GAR_ACO,FLD_GAR_CAL,FLD_GAR_CIU,FLD_GAR_COM,FLD_GAR_REG,FLD_GAR_FTAS,FLD_GAR_HIP,FLD_GAR_IBRA,FLD_GAR_IBRF,FLD_GAR_IBRN
-,FLD_GAR_NBO,FLD_GAR_NOT,FLD_GAR_NUE,FLD_GAR_ROLC1,FLD_GAR_ROLC2,FLD_GAR_SUCC,FLD_GAR_SUT,FLD_GAR_TGAR,FLD_GAR_TIB,FLD_GAR_VAT
-,FLD_GAR_VCO,FLD_GAR_VSIM,FLD_GAR_TIPO,FLD_GAR_MVEH,FLD_GAR_MODV, FLD_GAR_FEJE,FLD_GAR_TBI,FLD_GAR_TIC,FLD_GAR_DES
-,FLD_GAR_IBRC,FLD_GAR_TBIEN,FLD_GAR_MODELO,FLD_GAR_FIBR,FLD_GAR_BLOC,FLD_GAR_DEPTO,FLD_GAR_CBR,FLD_GAR_IBRA2,FLD_GAR_DIRN
-,FLD_GAR_CPOS,FLD_GAR_VMKD,FLD_GAR_POLI,FLD_GAR_MONB,FLD_GAR_FDEP,FLD_GAR_TCOMB,FLD_GAR_EASEG,FLD_GAR_NUMFAC,FLD_GAR_FEMFAC,FLD_GAR_MTOFAC
-       ,FLD_GAR_OTGR,FLD_GAR_BENL,FLD_GAR_ITEM 
-       FROM SCA_ADMINI..GAR INNER JOIN SCA_ADMINI..TCO ON FLD_GAR_OPER = FLD_TCO_OPER
-       WHERE FLD_GAR_OPER IN(${sourceOperation})
-       ${
-         exceptionsList.length > 0
-           ? `AND LTRIM(RTRIM(FLD_GAR_BLOC)) NOT IN(${exceptionsList.map(
-               (item) => `'${item}'`
-             )})`
-           : ''
-       }
-       ORDER BY  FLD_GAR_BLOC`
-              : ''
-          }
+          content={transferGoodsQuery(
+            sourceOperation,
+            targetOperation,
+            goodsList,
+            includeStatus
+          )}
         ></Query>
 
         {sourceOperation !== 0 && (
